01c4e2ac08f97b9198ecbcfc824126109e657d2b
Luke Griffiths <wlgriffiths@gmail.com>
2019-07-24 15:49:17 -0600

    [feat] before_encode and after_decode callback options

--- lib/json_serialized_attribute.rb
+++ lib/json_serialized_attribute.rb
@@ -15,11 +15,18 @@ module JsonSerializedAttribute
         :
           JSON.parse( saved_value )
 
+        if options[:after_decode]
+          output = options[:after_decode].call(output)
+        end
 
         output.is_a?(Hash) ? output.with_indifferent_access : output
       end
 
       define_method("#{attr_name}=") do |value|
+        if options[:before_encode]
+          value = options[:before_encode].call(value)
+        end
+
         write_attribute("#{attr_name}_serialized", value.to_json)
       end
     end
